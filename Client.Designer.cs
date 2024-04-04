170d10217f56cb24cb9eb30a4cafaf1f42681293
Är detta mitt namn? <87585550+OMGOMGplays@users.noreply.github.com>
2024-04-04 13:34:31 +0200

    Initial Commit

---
+++ Client.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ChatTCP
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ipConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ipInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ipConnect
+            // 
+            this.ipConnect.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ipConnect.Enabled = false;
+            this.ipConnect.Location = new System.Drawing.Point(524, 327);
+            this.ipConnect.Name = "ipConnect";
+            this.ipConnect.Size = new System.Drawing.Size(203, 42);
+            this.ipConnect.TabIndex = 0;
+            this.ipConnect.Text = "Connect To IP";
+            this.ipConnect.UseVisualStyleBackColor = true;
+            this.ipConnect.Click += new System.EventHandler(this.ipConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(503, 263);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please type in a valid TCP/IP address.";
+            // 
+            // ipInput
+            // 
+            this.ipInput.Location = new System.Drawing.Point(524, 286);
+            this.ipInput.Name = "ipInput";
+            this.ipInput.Size = new System.Drawing.Size(203, 22);
+            this.ipInput.TabIndex = 2;
+            this.ipInput.Tag = "";
+            this.ipInput.Text = "192.168.0.1";
+            this.ipInput.TextChanged += new System.EventHandler(this.ipInput_TextChanged);
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.ipInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ipConnect);
+            this.Name = "Client";
+            this.Text = "ChatTCP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ipConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ipInput;
+    }
+}
+
---
+++ Client.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatTCP
+{
+    public partial class Client : Form
+    {
+        public const int MAX_IP_LEN = 25;
+
+        public Client()
+        {
+            InitializeComponent();
+        }
+
+        private void ipConnect_Click(object sender, EventArgs e)
+        {
+            if (IsValidIP(ipInput.Text, out string output))
+            {
+                TryConnect(output);
+            }
+        }
+
+        private void ipInput_TextChanged(object sender, EventArgs e)
+        {
+            // Make sure the input doesn't exceed the allowed length
+            // I would freeze the text as it is, were I to know how to
+            if (ipInput.Text.Length > MAX_IP_LEN)
+            {
+                ipInput.Clear();
+            }
+
+            // Check if the IP is valid, if so, have the button be enabled
+            if (IsValidIP(ipInput.Text, out string output))
+            {
+                ipConnect.Enabled = true;
+            }
+            else // Otherwise, no button!
+            {
+                ipConnect.Enabled = false;
+            }
+        }
+
+        private bool IsValidIP(string input, out string output)
+        {
+            // Array of valid numbers, concat'd to one string to check if a valid int
+            char[] validNums = new char[MAX_IP_LEN];
+
+            // For each character in the input
+            for (int i = 0; i < input.Length; i++)
+            {
+                // Skip the .'s in e.g. "192.168.0.1"
+                if (input[i] == '.')
+                {
+                    i++;
+                    continue;
+                }
+                else // Otherwise, the input is a valid number
+                {
+                    validNums[i] = input[i];
+                }
+            }
+
+            // Output the resulting numbers
+            output = string.Concat(validNums);
+            // Return true if the output (validNums[]) is an int
+            return int.TryParse(output, out int result);
+        }
+
+        private bool TryConnect(string ip)
+        {
+            int intIP = int.Parse(ip);
+            Socket sock = new Socket(AddressFamily.Unspecified, SocketType.Stream, ProtocolType.Tcp);
+
+            return true;
+        }
+    }
+}
